5cd8c7a4702cced58d7509f3edddbe2d87097327
quocanh4433 <hoquocanh.td@gmail.com>
2023-03-03 16:48:11 +0700

    feat-register: validation

--- src/pages/Register/Register.tsx
+++ src/pages/Register/Register.tsx
@@ -1,40 +1,62 @@
+import { useForm } from 'react-hook-form';
 import { Link } from 'react-router-dom';
+import { getRules } from 'src/utils/rules';
+interface FormState {
+  email: string;
+  password: string;
+  confirm_password: string;
+}
 
 export default function Register() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors }
+  } = useForm<FormState>();
+
+  const rules = getRules();
+
+  const onSubmit = handleSubmit((data) => {
+    console.log(data);
+  });
+
   return (
     <section className='bg-orange'>
       <div className='mx-auto grid max-w-7xl grid-cols-1 bg-[url("https://cf.shopee.vn/file/sg-11134004-23020-hgx4adi96mnvbb")] bg-cover bg-no-repeat p-8 md:grid-cols-5 md:p-5 lg:py-40'>
-        <div className='rounded bg-white p-10 shadow-sm md:col-span-2 md:col-start-4'>
+        <div className='rounded bg-white py-10 px-8 shadow-sm md:col-span-3 md:col-start-4'>
           <h3 className='mb-6 text-xl'>Đăng ký</h3>
-          <form>
+          <form onSubmit={onSubmit}>
             <div>
               <input
                 type='text'
                 placeholder='Email'
                 className='w-full rounded border border-gray-300 px-4 py-2 outline-none focus:border-gray-400 focus:shadow'
+                {...register('email', rules.email)}
               />
-              <p className='mt-2: min-h-[1.7rem] text-xs text-red-700'>Email không đúng định dạng</p>
+              <p className='mt-2: min-h-[1.7rem] text-xs text-red-700'>{errors?.email?.message}</p>
             </div>
             <div>
               <input
                 type='password'
                 placeholder='Mật khẩu'
                 className='w-full rounded border border-gray-300 px-4 py-2 outline-none focus:border-gray-400 focus:shadow'
+                {...register('password', rules.password)}
               />
-              <p className='mt-2: min-h-[1.7rem] text-xs text-red-700'>Mật khẩu không đúng định dạng</p>
+              <p className='mt-2: min-h-[1.7rem] text-xs text-red-700'>{errors?.password?.message}</p>
             </div>
             <div>
               <input
                 type='password'
                 placeholder='Xác nhận mật khẩu'
                 className='w-full rounded border border-gray-300 px-4 py-2 outline-none focus:border-gray-400 focus:shadow'
+                {...register('confirm_password', rules.confirm_password)}
               />
-              <p className='mt-2: min-h-[1.7rem] text-xs text-red-700'>Mật khẩu không đúng định dạng</p>
+              <p className='mt-2: min-h-[1.7rem] text-xs text-red-700'>{errors?.confirm_password?.message}</p>
             </div>
 
             <button
-              type='button'
-              className='mt-5 mb-8 w-full rounded bg-orange py-2 px-4 text-center text-sm uppercase text-white'
+              type='submit'
+              className='mt-5 mb-8 w-full rounded bg-orange py-2 px-4 text-center uppercase text-white'
             >
               Đăng ký
             </button>
